8b0a320b4dd9897a8824b40f3615530bf9f840f4
Darshan Baral <darshanbaral@gmail.com>
2019-12-23 19:03:53 -0800

    Style updates

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Sidebar from "./components/sidebar";
+import Hamburger from "./components/hamburger";
 
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <Sidebar icon="_" content={<h2 style={{ padding: "10px" }}>Hi</h2>} />
+      <h1>Hamburger Menu</h1>
+      <h2>
+        By <a href="https://www.darshanbaral.com/">Darshan</a>.
+      </h2>
+      <h3>Fork here.</h3>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Hamburger size={50} colors={["black", "red"]} delay="0.5s" />
+      </div>
     </div>
   );
 }
