b62ddf0844a528bc4dc8ef1a06a619fd0ecf2a9a
Darshan Baral <darshanbaral@gmail.com>
2019-12-24 17:21:03 -0800

    Refactored to be class component

--- src/components/hamburger.js
+++ src/components/hamburger.js
@@ -1,80 +1,87 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Hamburger(props) {
-  const colors = props.colors
-    ? props.colors
-    : { hamburger: "black", cross: "black" };
-  const [isHamburger, setIsHamburger] = useState(true);
+class Hamburger extends React.Component {
+  state = { isHamburger: true };
 
-  const handleClick = () => {
-    if (props.alsoRun) {
-      props.alsoRun(!isHamburger);
+  handleClick = () => {
+    if (this.props.alsoRun) {
+      this.props.alsoRun(!this.state.isHamburger);
     }
-    setIsHamburger(!isHamburger);
+    this.setState({ isHamburger: !this.state.isHamburger });
   };
 
-  const size = props.size ? props.size : 25;
-  const lineThickness = size / 12;
-  const shiftLines = size / 2 - lineThickness;
-
-  return (
-    <div
-      style={{
-        width: `${size}px`,
-        height: `${size}px`,
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "space-between",
-        cursor: "pointer",
-        padding: "0"
-      }}
-      onClick={handleClick}
-    >
-      <hr
+  render() {
+    const colors = this.props.colors
+      ? this.props.colors
+      : { hamburger: "black", cross: "black" };
+    const size = this.props.size ? this.props.size : 25;
+    const lineThickness = size / 12;
+    const shiftLines = size / 2 - lineThickness;
+    return (
+      <div
         style={{
-          margin: "0",
-          border: `solid ${lineThickness}px`,
-          borderRadius: "calc(5px + 5%)",
-          borderColor: isHamburger ? colors.hamburger : colors.cross,
-          padding: "0",
-          width: "100%",
-          transform: isHamburger
-            ? "none"
-            : `translate(0, ${shiftLines}px) rotate(45deg)`,
-          transformOrigin: "50% 50%",
-          transition: `all ${props.delay}`
+          width: `${size}px`,
+          height: `${size}px`,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "space-between",
+          cursor: "pointer",
+          padding: "0"
         }}
-      />
-      <hr
-        style={{
-          margin: "0",
-          border: `solid ${lineThickness}px`,
-          borderRadius: "calc(5px + 5%)",
-          borderColor: isHamburger ? colors.hamburger : colors.cross,
-          padding: "0",
-          width: isHamburger ? "100%" : "0",
-          opacity: isHamburger ? "100%" : "0",
-          transition: `all ${props.delay}`
-        }}
-      />
-      <hr
-        style={{
-          margin: "0",
-          border: `solid ${lineThickness}px`,
-          borderRadius: "calc(5px + 5%)",
-          borderColor: isHamburger ? colors.hamburger : colors.cross,
-          padding: "0",
-          width: "100%",
-          transform: isHamburger
-            ? "none"
-            : `translate(0, -${shiftLines}px) rotate(-45deg)`,
-          transformOrigin: "50% 50%",
-          transition: `all ${props.delay}`
-        }}
-      />
-    </div>
-  );
+        onClick={this.handleClick}
+      >
+        <hr
+          style={{
+            margin: "0",
+            border: `solid ${lineThickness}px`,
+            borderRadius: "calc(5px + 5%)",
+            borderColor: this.state.isHamburger
+              ? colors.hamburger
+              : colors.cross,
+            padding: "0",
+            width: "100%",
+            transform: this.state.isHamburger
+              ? "none"
+              : `translate(0, ${shiftLines}px) rotate(45deg)`,
+            transformOrigin: "50% 50%",
+            transition: `all ${this.props.delay}`
+          }}
+        />
+        <hr
+          style={{
+            margin: "0",
+            border: `solid ${lineThickness}px`,
+            borderRadius: "calc(5px + 5%)",
+            borderColor: this.state.isHamburger
+              ? colors.hamburger
+              : colors.cross,
+            padding: "0",
+            width: this.state.isHamburger ? "100%" : "0",
+            opacity: this.state.isHamburger ? "100%" : "0",
+            transition: `all ${this.props.delay}`
+          }}
+        />
+        <hr
+          style={{
+            margin: "0",
+            border: `solid ${lineThickness}px`,
+            borderRadius: "calc(5px + 5%)",
+            borderColor: this.state.isHamburger
+              ? colors.hamburger
+              : colors.cross,
+            padding: "0",
+            width: "100%",
+            transform: this.state.isHamburger
+              ? "none"
+              : `translate(0, -${shiftLines}px) rotate(-45deg)`,
+            transformOrigin: "50% 50%",
+            transition: `all ${this.props.delay}`
+          }}
+        />
+      </div>
+    );
+  }
 }
 
 export default Hamburger;
